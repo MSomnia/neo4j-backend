a60ebf0d022c03b5cd76510859e2827c32852091
Yi Xiao <yxiao442@uwo.ca>
2024-11-21 13:42:48 -0500

    Latest Version

---
+++ pages/api/getTagOfNote.js
@@ -0,0 +1,22 @@
+import { runQuery } from "../../lib/neo4j";
+
+export default async function handler(req, res) {
+    if (req.method === 'GET') {
+        const { note_id } = req.body;
+        if(!note_id){
+            return res.status(400).json({ error: 'Missing data' });
+        }
+
+        const cypher = 'MATCH(n:note{id:$note_id})-[:CONTAINS]->(t:tag) RETURN t.name AS note_tag';
+        try {
+            const data = await runQuery(cypher,{note_id});
+            res.status(200).json({success: true, data});
+        } catch (error) {
+            console.error('Error fetching data:', error);
+            res.status(500).json({success: false, message: 'Failed to fetch data', error: error.message});
+        }
+    } else {
+        res.setHeader('Allow', ['GET']);
+        res.status(405).json({success: false, message: `Method ${req.method} Not Allowed`});
+    }
+}
