0971f8420ed7fbaf097748eb3d74cfffdb291a13
MSomnia <linleihao414@outlook.com>
2024-11-28 14:46:33 -0500

    update getANote

--- pages/api/getANote.js
+++ pages/api/getANote.js
@@ -2,14 +2,16 @@ import { runQuery } from "../../lib/neo4j";
 
 export default async function handler(req, res) {
     if (req.method === 'GET') {
-        const { note_id } = req.body;
+        const note_id = req.query.note_id
         if(!note_id){
            return res.status(400).json({ error: 'Missing data' });
         }
 
-        const cypher = 'MATCH(u:user)-[:OWNED]->(n:note{id:$note_id})-[:TAKEN_IN]->(c:course),' +
-            '(n:note{id:$note_id})-[:CONTAINS]->(t:tag)RETURN n.id AS note_id, n.title AS note_title,n.content AS note_content,' +
-            'c.title AS course_of_note,COLLECT(t.name) AS tag_name,u.name AS author_of_note, n.date AS note_created_date';
+        const cypher = `
+        MATCH(u:user)-[:OWNED]->(n:note{id:$note_id})-[:TAKEN_IN]->(c:course),
+        (n:note{id:$note_id})-[:CONTAINS]->(t:tag) RETURN n.id AS note_id, n.title AS note_title,n.content AS note_content,
+        c.title AS course_of_note,COLLECT(t.name) AS tag_name,u.name AS author_of_note, n.date AS note_created_date
+            `;
         try {
             const data = await runQuery(cypher,{note_id});
             data.forEach((note)=>{
